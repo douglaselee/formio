2d3fe7e8fb38387d0d53c781336d88b95f051889
douglaselee <douglaselee@comcast.net>
2017-02-21 16:45:30 -0500

    Add support for SSL

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 'use strict';
 
+var fs      = require('fs');
+var http    = require('http');
+var https   = require('https');
+var options = {
+  key:  fs.readFileSync('./ssl/server.key'),
+  cert: fs.readFileSync('./ssl/server.crt')
+};
+
 var util = require('./src/util/util');
 require('colors');
 require('./server')().then(function(state) {
   util.log(' > Serving the Form.io API Platform at ' + state.config.domain.green);
-  state.server.listen(state.config.port);
+//state.server.listen(state.config.port);
+  http.createServer(state.server).listen(state.config.port);
+  https.createServer(options, state.server).listen(state.config.sslPort);
 });
