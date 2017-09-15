71073b69e97d8eb8a10e7d2a7658df832091cd86
douglaselee <douglaselee@comcast.net>
2017-09-15 11:30:17 -0400

    Support SSL and configured host and port when server is run as a service

--- service.js
+++ service.js
@@ -49,11 +49,20 @@ else if (process.argv[2] === "--run") {
         service.stop(0);
     });
 
+    var http    = require('http');
+    var https   = require('https');
+    var sslOpts = {
+        key:  fs.readFileSync('./ssl/server.key'),
+        cert: fs.readFileSync('./ssl/server.crt')
+    };
+
     var util = require('./src/util/util');
     require('colors');
     require('./server')().then(function(state) {
         util.log(' > Serving the Form.io API Platform at ' + state.config.domain.green);
-        state.server.listen(state.config.port);
+      //state.server.listen(state.config.port);
+        http.createServer(state.server).listen(state.config.port, state.config.host);
+        https.createServer(sslOpts, state.server).listen(state.config.sslPort, state.config.host);
     });
 }
 else {
