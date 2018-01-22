7f5dcba272270b0579f3245cd6a4a23f65db9102
douglaselee <douglaselee@comcast.net>
2018-01-22 14:41:23 -0500

    Added placeholder hooks

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 var fs      = require('fs');
 var http    = require('http');
 var https   = require('https');
+var hooks   = require('./hooks');
 var options = {
   key:  fs.readFileSync('./ssl/server.key'),
   cert: fs.readFileSync('./ssl/server.crt')
@@ -10,7 +11,7 @@ var options = {
 
 const util = require('./src/util/util');
 require('colors');
-require('./server')().then(function(state) {
+require('./server')(hooks).then(function(state) {
   util.log(` > Serving the Form.io API Platform at ${state.config.domain.green}`);
 //state.server.listen(state.config.port);
   http.createServer(state.server).listen(state.config.port, state.config.host);
