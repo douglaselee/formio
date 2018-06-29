4152e3dbc07fd89ccf94e0b2759590705eeb2e74
douglaselee <douglaselee@comcast.net>
2018-06-29 15:49:09 -0400

    Start adding ftp server

--- main.js
+++ main.js
@@ -17,3 +17,30 @@ require('./server')(hooks).then(function(state) {
   http.createServer(state.server).listen(state.config.port, state.config.host);
   https.createServer(options, state.server).listen(state.config.sslPort, state.config.host);
 });
+
+const tls       = require('tls');
+const opt       = {
+  tls: {
+    key:  './ssl/ftp/server.key',
+    cert: './ssl/ftp/server.crt',
+    ca:   './ssl/ftp/server.csr'
+  }
+};
+
+const context   = tls.createSecureContext(opt);
+const FtpSrv    = require('ftp-srv');
+//nst ftpServer = new FtpSrv('ftps://0.0.0.0:990', opt);
+const ftpServer = new FtpSrv('ftp://0.0.0.0:21', {});
+
+ftpServer.on('login', function(data, resolve, reject) {
+//return reject('WTF!');
+  resolve({ root: '.', cwd: '.' });
+});
+
+ftpServer.listen().then(function() {
+  var v = 1;
+});
+
+ftpServer.on('client-error', function(connection, context, error) {
+  var v = 1;
+});
